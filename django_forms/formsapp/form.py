0c8726d832720aedd879538f4ab7b860bf95190e
Keylogger-hash <example@example.com>
2024-04-26 00:47:26 +0500

    Django forms

---
+++ django_forms/formsapp/form.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class StudentForm(forms.Form):
+    name = forms.CharField(label='Full name', max_length=40)
+    email = forms.EmailField(label='Email', max_length=50)
+    username = forms.CharField(label='Username', max_length=20)
+    password = forms.CharField(label='Password', min_length=10, max_length=20, widget=forms.PasswordInput)
