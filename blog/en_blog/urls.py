130d483b526b2b81ebbd8a2951b7f0ef808bb19d
Keylogger-hash <example@example.com>
2024-04-26 00:32:41 +0500

    Блог апи

--- blog/en_blog/urls.py
+++ blog/en_blog/urls.py
@@ -1,10 +1,13 @@
 from . import views 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from django.urls import path 
-
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='post')
  
 
 urlpatterns = [ 
-    path('', views.PostList.as_view(), name='home'), 
+    #path('', views.PostList.as_view(), name='home'), 
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'), 
+    path('api/v1/',include(router.urls))
 ] 
